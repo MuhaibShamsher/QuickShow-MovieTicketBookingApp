02f905f8e580d949701795865b4e6150de51ecc6
Muhaib Shamsher <muhaibshamsher9999@gmail.com>
2025-06-21 21:34:07 +0500

    Navigation Component

--- Client/src/components/NavBar.jsx
+++ Client/src/components/NavBar.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets.js'
-import { SearchIcon, MenuIcon, XIcon } from 'lucide-react'
+import { SearchIcon, MenuIcon, XIcon, User, TicketPlus } from 'lucide-react'
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
 function NavBar() {
+  const navigate = useNavigate()
   const [isOpen, setIsOpen] = useState(false)
+  const { user } = useUser()
+  const { openSignIn } = useClerk()
 
   return (
     <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5' >
@@ -12,21 +16,40 @@ function NavBar() {
         <img src={assets.logo} alt='' className='w-36 h-auto' />
       </Link>
 
-      <div className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-lg z-50 flex flex-col md:flex-row items-center max-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 overflow-hidden transition-[width] duration-300 ${isOpen? 'max-md:w-full' : 'max-md:w-0'}`}>
+      <div className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-lg z-50 flex flex-col md:flex-row items-center max-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 overflow-hidden transition-[width] duration-300 ${isOpen ? 'max-md:w-full' : 'max-md:w-0'}`}>
         <XIcon
           className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer4'
           onClick={() => setIsOpen(!isOpen)}
         />
-        <Link to='/' onClick={() => {scroll(0,0), setIsOpen(false)}}>Home</Link>
-        <Link to='/movies' onClick={() => {scroll(0,0), setIsOpen(false)}}>Movies</Link>
-        <Link to='/' onClick={() => {scroll(0,0), setIsOpen(false)}}>Theaters</Link>
-        <Link to='/' onClick={() => {scroll(0,0), setIsOpen(false)}}>Releases</Link>
-        <Link to='/favourite' onClick={() => {scroll(0,0), setIsOpen(false)}}>Favourites</Link>
+        <Link to='/' onClick={() => { scroll(0, 0), setIsOpen(false) }}>Home</Link>
+        <Link to='/movies' onClick={() => { scroll(0, 0), setIsOpen(false) }}>Movies</Link>
+        <Link to='/' onClick={() => { scroll(0, 0), setIsOpen(false) }}>Theaters</Link>
+        <Link to='/' onClick={() => { scroll(0, 0), setIsOpen(false) }}>Releases</Link>
+        <Link to='/favourite' onClick={() => { scroll(0, 0), setIsOpen(false) }}>Favourites</Link>
       </div>
 
       <div className='flex items-center gap-8'>
         <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer' />
-        <button className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>Login</button>
+        {
+          !user ? (
+            <button
+              className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'
+              onClick={openSignIn}
+            >
+              Login
+            </button>
+          ) : (
+            <UserButton>
+              <UserButton.MenuItems>
+                <UserButton.Action
+                  label='My Bookings'
+                  labelIcon={<TicketPlus width={15} />}
+                  onClick={() => navigate('/my-bookings')}
+                />
+              </UserButton.MenuItems>
+            </UserButton>
+          )
+        }
       </div>
 
       <MenuIcon
