ae39beed5f49ac4522d798ab1c1d1af64cb85a1c
Muhaib Shamsher <muhaibshamsher9999@gmail.com>
2025-06-28 21:52:35 +0500

    Add Toaster component to Layout

--- Client/src/Layout.jsx
+++ Client/src/Layout.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { Outlet, useLocation } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
+import { Toaster } from 'react-hot-toast'
 
 function Layout() {
     const isAdminRoute = useLocation().pathname.startsWith('/admin')
 
   return (
     <>
-       {!isAdminRoute && <NavBar />}
-       <Outlet />
-       {!isAdminRoute && <Footer />}
+      <Toaster />
+      {!isAdminRoute && <NavBar />}
+      <Outlet />
+      {!isAdminRoute && <Footer />}
     </>
   )
 }
